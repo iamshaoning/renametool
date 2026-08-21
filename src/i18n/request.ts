e08a6eb93deb558e3d1db2716879cabce5c4812e
iamshaoning <825824748@qq.com>
2026-08-21 22:05:01 +0800

    清理：移除测试套件与无用配置，合并 i18n 路由

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,13 +1,6 @@
-import { hasLocale } from "next-intl";
 import { getRequestConfig } from "next-intl/server";
-import { routing } from "./routing";
 
-export default getRequestConfig(async ({ requestLocale }) => {
-	const requested = await requestLocale;
-	const locale = hasLocale(routing.locales, requested) ? requested : routing.defaultLocale;
-
-	return {
-		locale,
-		messages: (await import(`../../messages/${locale}.json`)).default,
-	};
-});
+export default getRequestConfig(async () => ({
+	locale: "zh",
+	messages: (await import("../../messages/zh.json")).default,
+}));
